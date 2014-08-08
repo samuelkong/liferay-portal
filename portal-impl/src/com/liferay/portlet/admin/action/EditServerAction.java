7b16c8269951ccd96003b02373a6703afc4d3b28
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2014-08-08 16:16:58 +0200

    LPS-49221 Allow CRLF for Administration Script Console

--- portal-impl/src/com/liferay/portlet/admin/action/EditServerAction.java
+++ portal-impl/src/com/liferay/portlet/admin/action/EditServerAction.java
@@ -519,7 +519,7 @@ public class EditServerAction extends PortletAction {
 			SessionErrors.add(
 				actionRequest, ScriptingException.class.getName(), se);
 
-			_log.error(se.getMessage());
+			SanitizerLogWrapper.allowCRLF(_log).error(se.getMessage());
 		}
 	}
 
