25faec2e5cba44925ea87662b0b79cd48acd17d4
Yuxing Wu <yuxing.wu@liferay.com>
2015-03-03 10:24:34 +0800

    LPS-51404 add testGetDomain() in DomainImplTest

--- portal-impl/test/unit/com/liferay/portal/util/DomainImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/DomainImplTest.java
@@ -14,8 +14,50 @@
 
 package com.liferay.portal.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author Yuxing Wu
  */
 public class DomainImplTest {
+
+	@Test
+	public void testGetDomain() {
+		Assert.assertNull(_domainImpl.getDomain(null));
+
+		Assert.assertEquals("127.0.0.1", _domainImpl.getDomain("127.0.0.1"));
+
+		Assert.assertNull(_domainImpl.getDomain("com"));
+
+		Assert.assertEquals(
+			".liferay.com", _domainImpl.getDomain("liferay.com"));
+
+		Assert.assertEquals(
+			".liferay.com", _domainImpl.getDomain("www.liferay.com"));
+
+		Assert.assertEquals(
+			".cdn.liferay.com", _domainImpl.getDomain("www.cdn.liferay.com"));
+
+		Assert.assertEquals(
+			".liferay.qld.gov.au", _domainImpl.getDomain("liferay.qld.gov.au"));
+
+		Assert.assertEquals(
+			".liferay.qld.gov.au",
+			_domainImpl.getDomain("www.liferay.qld.gov.au"));
+
+		Assert.assertEquals(
+			".cdn.liferay.qld.gov.au",
+			_domainImpl.getDomain("www.cdn.liferay.qld.gov.au"));
+
+		Assert.assertNull(_domainImpl.getDomain("localhost"));
+
+		Assert.assertEquals(".test", _domainImpl.getDomain("liferay.test"));
+
+		Assert.assertEquals(
+			".liferay.test", _domainImpl.getDomain("www.liferay.test"));
+	}
+
+	private final DomainImpl _domainImpl = new DomainImpl();
+
 }
