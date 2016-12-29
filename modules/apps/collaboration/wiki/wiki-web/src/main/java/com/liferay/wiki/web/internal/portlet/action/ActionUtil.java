3b1db91e8ead5469e79b93f735a0cb3a5fc023ac
arthurchan35 <arthurchan35@gmail.com>
2016-12-29 16:55:44 +0800

    LPS-363362 wiki to handle tags properly

--- modules/apps/collaboration/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/portlet/action/ActionUtil.java
+++ modules/apps/collaboration/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/portlet/action/ActionUtil.java
@@ -416,8 +416,9 @@ public class ActionUtil {
 		}
 
 		long categoryId = ParamUtil.getLong(renderRequest, "categoryId");
+		String tagName = ParamUtil.getString(renderRequest, "tag");
 
-		if (categoryId > 0) {
+		if (categoryId > 0 || !Validator.isNull(tagName)) {
 			return "/wiki/view_categorized_pages.jsp";
 		}
 		else {
